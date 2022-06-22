38698a0d19b8eddf5eab939ce31d1e1745de9fe4
tomelovich <tomelovichs@gmail.com>
2022-06-22 11:21:46 +0300

    Названия по ошибке слетели (должно быть 31)

---
+++ 32/z1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace z1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.btnFill = new System.Windows.Forms.Button();
+            this.dataSet1 = new System.Data.DataSet();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // btnFill
+            // 
+            this.btnFill.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnFill.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnFill.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnFill.Location = new System.Drawing.Point(543, 400);
+            this.btnFill.Name = "btnFill";
+            this.btnFill.Size = new System.Drawing.Size(245, 64);
+            this.btnFill.TabIndex = 2;
+            this.btnFill.Text = "Добавить";
+            this.btnFill.UseVisualStyleBackColor = false;
+            this.btnFill.Click += new System.EventHandler(this.btnFill_Click);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(803, 177);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(0, 183);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(803, 177);
+            this.dataGridView2.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 476);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnFill);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button btnFill;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+    }
+}
+
---
+++ 32/z1/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using MongoDB.Driver.Core.Configuration;
+using MySql.Data.MySqlClient;
+using System.Collections;
+using System.Data.Common;
+using System.Data.SqlClient;
+
+namespace z1
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection _connection = new MySqlConnection(
+            "server=localhost;username=mysql;database=dbtur_firm");
+
+        public string sql;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFill_Click(object sender, EventArgs e)
+        {
+            _connection.Open();
+
+            printTour();
+            printTourist();
+
+            _connection.Close();
+        }
+
+
+        public void printTour()
+        {
+            sql = "SELECT * FROM `туры`";
+            MySqlCommand command = new MySqlCommand(sql, _connection);
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            DataSet ds = new DataSet();
+
+            adapter.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        public void printTourist()
+        {
+            sql = "SELECT * FROM `туристы`";
+            MySqlCommand command = new MySqlCommand(sql, _connection);
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            DataSet ds = new DataSet();
+
+            adapter.Fill(ds);
+            dataGridView2.DataSource = ds.Tables[0];
+        }
+
+        public void deleteTour()
+        {
+            sql = "DELETE FROM `тур` WHERE `код тура` = 2 LIMIT 1";
+            MySqlCommand command = new MySqlCommand(sql, _connection);
+
+            command.ExecuteNonQuery();
+        }
+
+        public void addTourist()
+        {
+            var ID = 3;
+            var _surname = "Дуров";
+            var _name = "Павел";
+            var _sname = "Валерьевич";
+
+            string _sql = string.Format("INSERT INTO Туристы (Код, Фамилия, Имя, Отчество)" +
+                "VALUES (@ID, @_surname, @_name, @_sname)");
+
+            MySqlCommand _command = new MySqlCommand(_sql, _connection);
+
+            _command.Parameters.AddWithValue("@ID", ID);
+            _command.Parameters.AddWithValue("@_surname", _surname);
+            _command.Parameters.AddWithValue("@_name", _name);
+            _command.Parameters.AddWithValue("@_sname", _sname);
+
+            _command.ExecuteNonQuery();
+        }
+
+        public void updateTourist()
+        {
+            string _sql = "UPDATE Туристы SET Имя='Виталий' WHERE Код=2";
+            MySqlCommand _command = new MySqlCommand(_sql, _connection);
+
+            _command.ExecuteNonQuery();
+        }
+    }
+}
